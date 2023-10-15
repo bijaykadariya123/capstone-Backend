afed4dcb0caea3699624d7da8dbb8c9d4ee9c34a
Bijay Kadariya <bijaykadariya92@gmail.com>
2023-10-15 16:29:48 -0400

    migration done

--- capstone_shoppingcart/settings.py
+++ capstone_shoppingcart/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'rest_framework'
+    'rest_framework',
+    'shoppify.apps.ShoppifyConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -84,11 +87,12 @@ WSGI_APPLICATION = 'capstone_shoppingcart.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
+
 
 
 # Password validation
